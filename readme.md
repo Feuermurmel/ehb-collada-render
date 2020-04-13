f4a07ca298943cb762116ef32c9f1d9e1852c09a
Michael Schwarz <michi.schwarz@gmail.com>
2020-04-13 22:17:28 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# collada-render
+
+## Development Setup
+
+```
+python3 -m venv venv
+. venv/bin/activate
+pip install -e '.[dev]'
+```
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+name = 'collada-render'
+dependencies = []
+dependencies_dev = ['pytest']
+console_scripts = ['collada-render = colladarender:entry_point']
+
+setuptools.setup(
+    name=name,
+    entry_points=dict(console_scripts=console_scripts),
+    install_requires=dependencies,
+    extras_require={'dev': dependencies_dev},
+    setup_requires=['setuptools>=42', 'wheel', 'setuptools_scm[toml]>=3.4'],
+    use_scm_version=True,
+    include_package_data=True,
+    packages=setuptools.find_packages(exclude=['tests']))
